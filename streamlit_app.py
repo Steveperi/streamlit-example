999b1fa39d154645ac27fccc9c795c50a6d754a1
Steveperi <luras.periklis.1985@gmail.com>
2021-03-19 17:32:48 +0200

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,19 @@ with st.echo(code_location='below'):
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
         dataframe = pd.read_csv(uploaded_file)
-        print(dataframe[dataframe.columns[0]])
-        st.write(dataframe)
-        st.line_chart(dataframe)
-        st.area_chart(dataframe)
+        first = 0
+        colCurrent = ''
+        for col in dataframe.columns:
+            if first == 0:
+                print(col)
+                colCurrent = col
+            else:
+                c = alt.Chart(dataframe).mark_bar().encode(x=colCurrent, y=col)
+                st.altair_chart(c, use_container_width=True)
+            first = first + 1
+        # st.altair_chart(c, use_container_width=True)
+        # c = alt.Chart(dataframe).mark_bar().encode(x='Sites', y=' Views')
+        # st.altair_chart(c, use_container_width=True)
+        # st.write(dataframe)
+        # st.line_chart(dataframe)
+        # st.area_chart(dataframe)
